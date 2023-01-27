c58ed4997b4e9572e5265d1b8c6671356b61e5af
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-01-27 10:12:22 +0000

    Updated observer to make use of reworked detDyn package

--- lorenz96.py
+++ lorenz96.py
@@ -1,27 +1,25 @@
 """
 Model Definition for the L96 model. Based on equation 1 of Lorenz's 2005 paper (https://doi.org/10.1175/JAS3430.1)
 """
-
 # Imports
-from detDyn.dynamics.integrator import odeIntegrator
-from detDyn.dynamics.observers import baseObserver
+from detDyn.integrator import OdeIntegrator
+from detDyn.observers.xarray import TrajectoryObserver
 
 import numpy as np
 import numpy.random as rm
 import xarray as xr
-from tqdm import tqdm
 
 # Standard Parameter Choice based on page 1577 in Lorenz 2005.
-F = 10
-N = 30
+F = 8
+N = 50
 
 
-def l96_rhs(x, F=10, N=30):
+def l96_rhs(x, F=8, N=50):
     return -np.roll(x, -1) * (np.roll(x, -2) - np.roll(x, 1)) - x + F
 
 
-class l96Integrator(odeIntegrator):
-    def __init__(self, F=10, N=30, ic=None):
+class L96Integrator(OdeIntegrator):
+    def __init__(self, F=8, N=50, ic=None):
 
         self.F = F
         self.N = N
@@ -33,27 +31,10 @@ class l96Integrator(odeIntegrator):
         super().__init__(l96_rhs, ic, parameters={"F": F, "N": N})
 
 
-class l96TrajectoryObserver(baseObserver):
+class L96TrajectoryObserver(TrajectoryObserver):
     def __init__(self, l96_integrator):
 
         super().__init__(l96_integrator)
-
-    def look(self, integrator):
-        """Observes trajectory of L96 trajectory"""
-
-        # Note the time
-        self._time_obs.append(integrator.time)
-
-        # Making Observations
-        self._observations.append(integrator.state.copy())
-        return
-
-    def make_observations(self, number, frequency, timer=True):
-        self.look(self.integrator)  # Make a note of IC
-        for x in tqdm(range(number), disable=not timer):
-            self.integrator.run(frequency)
-            self.look(self.integrator)
-        return
 
     @property
     def observations(self):
